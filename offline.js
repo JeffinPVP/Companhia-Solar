b76cb22a2509b06c3a5651a95e382884aad9a2bc
JeffinPVP <jeffersonpassarelli2010@gmail.com>
2025-11-30 15:49:21 -0300

    okay

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1764528152,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/telafundo-sheet0.png",
+		"images/fundoloja-sheet0.png",
+		"images/prodenerg-sheet0.png",
+		"images/prodenerg-sheet1.png",
+		"images/linhas-sheet0.png",
+		"images/fundo3-sheet0.png",
+		"images/telabanco.png",
+		"images/telaempréstimo.png",
+		"images/fundo4-sheet0.png",
+		"images/fundojogo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
